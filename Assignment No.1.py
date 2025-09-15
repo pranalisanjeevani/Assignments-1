d225f2b13ec0c917a9001f671226786f3b0bc900
pranalisanjeevani <pranalisanjeevanisanjay@gmail.com>
2025-09-15 10:46:33 +0530

    Add files via upload

---
+++ Assignment No.1.py
@@ -0,0 +1,14 @@
+# Task 1: Perform Basic Mathematical Operations
+
+a=float(input("Enter the First Number :"))
+b=float(input("Enter the Second Number :"))
+print("Addition :", a+b)
+print("Subtraction :", a-b)
+print("Multiplication :", a*b)
+print("Division :", a/b)
+
+# Task 2 :- Create a Personalized Greeting
+
+a=input("Enter your First Name: ")
+b=input("Enter your Last Name: ")
+print("Hello ",a  +  b,"! Welcome to the Python World")
